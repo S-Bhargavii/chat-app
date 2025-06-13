8f4cecdcb777f1a69cbf3c4ee7017c51fc19e193
S-Bhargavii <bhargavi005@e.ntu.edu.sg>
2025-06-13 21:21:07 +0800

    initialise frontend and backend

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import express from "express" // this imports the express libary and the import is assigned to express variable 
+import router from "./routes/auth.route.js";
+
+const app = express(); 
+
+app.use("/api/auth", authRoutes);
+
+app.listen(
+    5001,
+    ()=>{
+        console.log("server is running on port 5001");
+    }
+)
---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,9 @@
+import express from "express"
+
+const router = express.Router()
+
+// what code must be executed at the signup endpoint
+router.get("/signup", 
+    
+);
+export default router;
