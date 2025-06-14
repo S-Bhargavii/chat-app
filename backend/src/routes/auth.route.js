0ce3384f43a4ff031be5b764005c9f9d17f324bc
S-Bhargavii <bhargavi005@e.ntu.edu.sg>
2025-06-14 08:07:11 +0800

    move logic to controllers

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,24 +1,13 @@
 import express from "express"
+import { signup, login, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router()
 
 // what code must be executed at the signup endpoint
-router.get("/signup", 
-    (req, res) => {
-        res.send("signup route");
-    }
-);
+router.get("/signup", signup);
 
-router.get("/login", 
-    (req, res) => {
-        res.send("login route");
-    }
-);
+router.get("/login", login);
 
-router.get("/logout", 
-    (req, res) => {
-        res.send("logout route");
-    }
-);
+router.get("/logout", logout);
 
 export default router;
