aa82d31e534a3649b0ad7a445f6c6072f03d81b0
S-Bhargavii <bhargavi005@e.ntu.edu.sg>
2025-06-14 10:21:10 +0800

    add ignore and .env file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,16 @@
 import express from "express" // this imports the express libary and the import is assigned to express variable 
 import authRoutes from "./routes/auth.route.js" ;
+import dotenv from "dotenv";
 
 const app = express(); 
+dotenv.config(); 
 
+const port = process.env.PORT
 app.use("/api/auth", authRoutes);
 
 app.listen(
-    5001,
+    port,
     ()=>{
-        console.log("server is running on port 5001");
+        console.log(`server is running on port :` + port);
     }
 )
