9758ae7b5304a743e2d0c8ca4ecaa2735a24ee6d
S-Bhargavii <bhargavi005@e.ntu.edu.sg>
2025-06-14 14:01:19 +0800

    add send message functionality

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUsersForSidebar = async(req, res) => {
     try{
@@ -29,6 +30,36 @@ export const getMessages = async(req, res) => {
 
     }catch(error){
         console.error("Error in getmessages: ", error.message);
+        return res.status(500).json({error: "Internal server error occured"});
+    }
+}
+
+export const sendMessage = async(req, res) => {
+    try{
+        const {id:toUserId} = req.params;
+        const fromUserId = req.user._id;
+        const {text, image} = req.body;
+
+        let imageUrl;
+        if(image){
+            // upload image to cloudinary 
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId: fromUserId, 
+            recieverId: toUserId, 
+            text: text,
+            image: imageUrl
+        })
+        await newMessage.save(); // save to mongodb database
+
+        // to do : socket.io
+        
+        return res.json(200).json(newMessage);
+    }catch(error){
+        console.error("Error occured while sending images: ", error.message);
         return res.status(500).json({error: "Internal server error occured"});
     }
 }
