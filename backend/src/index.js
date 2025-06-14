603ff40967307079fa5cb47dda1eb8d8fac40b49
S-Bhargavii <bhargavi005@e.ntu.edu.sg>
2025-06-14 10:53:08 +0800

    add user model and schema

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@
 import express from "express"
 import authRoutes from "./routes/auth.route.js" ;
 import dotenv from "dotenv";
-import { connectDB } from "./models/db.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express(); 
 dotenv.config(); 
