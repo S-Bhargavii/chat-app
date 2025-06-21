65631b22abbd8837dabd8f973912db308984d099
S-Bhargavii <bhargavi005@e.ntu.edu.sg>
2025-06-21 21:09:29 +0800

    add starter chat container UI and add chatheader component

--- frontend/src/pages/ChatContainer.jsx
+++ frontend/src/pages/ChatContainer.jsx
@@ -1,8 +1,30 @@
+import { ChatHeader } from "../components/ChatHeader";
+import MessageInput from "../components/MessageInput";
+import { useChatStore } from "../store/useChatStore"
+import { useEffect } from "react";
 
 const ChatContainer = () => {
+  const {selectedUser, messages, getMessages, isMessagesLoading} = useChatStore();
+
+  useEffect(()=>{
+    getMessages(selectedUser._id);
+    // get the messages made with the selected user 
+    // whenever the user changes and when the component is first mounted
+  }, [selectedUser._id, getMessages]);
+
+  if(isMessagesLoading) return <div>Loading..</div>
+
   return (
-    <div>ChatContainer</div>
+    <div className="flex-1 flex flex-col overflow-auto">
+      {/* chat header --> selected user profile picture, selected user id */}
+      <ChatHeader/>
+      {/* messages with the selected user */}
+      <p>Messages </p>
+      {/* input to send new messages */}
+      <MessageInput/>
+    </div>
   )
+
 }
 
 export default ChatContainer
