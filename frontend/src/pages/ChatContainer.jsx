fc9b796613d6e9b7d0d9eea924e7846c0fcbd6b9
S-Bhargavii <bhargavi005@e.ntu.edu.sg>
2025-06-22 01:42:05 +0800

    display messages

--- frontend/src/pages/ChatContainer.jsx
+++ frontend/src/pages/ChatContainer.jsx
@@ -3,9 +3,11 @@ import MessageInput from "../components/MessageInput";
 import { useChatStore } from "../store/useChatStore"
 import { useEffect } from "react";
 import MessageSkeleton from "../components/skeletons/MessageSkeleton";
+import { useAuthStore } from "../store/useAuthStore";
 
 const ChatContainer = () => {
   const {selectedUser, messages, getMessages, isMessagesLoading} = useChatStore();
+  const {authUser} = useAuthStore();
 
   useEffect(()=>{
     getMessages(selectedUser._id);
@@ -26,7 +28,42 @@ const ChatContainer = () => {
       {/* chat header --> selected user profile picture, selected user id */}
       <ChatHeader/>
       {/* messages with the selected user */}
-      <p>Messages </p>
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.map((message) => (
+          <div
+            key = {message._id}
+            className={`chat ${message.senderId === authUser._id ?  "chat-end" :  "chat-start"}`}
+          >
+            {/* display sender's profile pic */}
+            <div className="chat-image avatar">
+              <div className="size-10 rounded-full-border">
+                <img
+                  src = {message.senderId === authUser._id ? authUser.profilePic || "/avatar.jpg" : selectedUser.profilePic || "/avatar.jpg"}
+                  alt="profile pic"
+                />
+              </div>
+            </div>
+            
+            {/* show the time message was sent */}
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {message.createdAt}
+              </time>
+            </div>
+
+            <div className="chat-bubble flex">
+              {message.image && (
+                <img 
+                  src = {message.image}
+                  alt = "Attachment"
+                  className = "sm:max-w-[200px] rounded-md mb-2"
+                />
+              )}
+              {message.text && <p>{message.text}</p>}
+            </div>
+          </div>
+        ))}
+      </div>
       {/* input to send new messages */}
       <MessageInput/>
     </div>
