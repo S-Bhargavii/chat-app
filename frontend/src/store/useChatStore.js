83a417bdba5542292ba8a485c4fce368a11ccddb
S-Bhargavii <bhargavi005@e.ntu.edu.sg>
2025-06-22 00:20:43 +0800

    add file upload and text inputs in MessageInput component

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,10 +2,16 @@ import {create} from "zustand";
 import toast from "react-hot-toast";
 import {axiosInstance} from "../lib/axios";
 
-export const useChatStore = create((set)=> ({
-    messages : [], 
-    users: [], 
-    selectedUser: null, 
+// create is a function that is taking in a function as an argument 
+// so the create function will pass in the set and get args to the function 
+// it is calling inside --> idhi argument kindha icham kadha -> adhi anamata 
+
+// create returns a callback which is assigned to useChatStore 
+// which we invoke by calling useChatStore();
+export const useChatStore = create((set, get)=> ({
+    messages : [], // the list of all the messages made with the user that we are texting
+    users: [], // the list of all the users to display on the sidebar
+    selectedUser: null, // the user that we are texting with
     isUsersLoading: false, 
     isMessagesLoading: false, 
 
@@ -33,6 +39,22 @@ export const useChatStore = create((set)=> ({
             set({isMessagesLoading : false});
         }
     }, 
+
+    sendMessage : async(message) => {
+        const {selectedUser, messages} = get(); // the get method provided in the argument 
+        // lets you to access any object within this state that we are creating
+        // because it is in the object --> we can't use it normally like a variable 
+        // but we need use the get() and set() methods they provide 
+        // to read and write to the variable 
+        try{
+            // send the message to the backend api so that it can store in the database 
+            res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, message);
+            // append the message to the list of messages in the frontend
+            set({messages : [...messages, res.data]});
+        } catch(error){
+            toast.error(error.response.data.message);
+        }
+    },
 
     setSelectedUser: (selectedUser) => {
         set({selectedUser})
